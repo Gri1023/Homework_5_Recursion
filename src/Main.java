709e29f9336e61fb119169c26fcf32daffe2e9ce
Gri1023 <hryhorii.bavykin@gmail.com>
2023-01-11 14:58:33 +0200

    finally working fine!!!

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,2,1};
+        int[] arr = {1,2,4,5,5,4,2,1};
         int start = 0;
         int end = arr.length-1;
-        System.out.println ("end =" + end/2);
-        if (checkpalindrome(arr,start,end) == 1) {
-            System.out.println("this array is palindrome");
+        if (checkPalindrome(arr,start,end) == 1) {
+            System.out.println("Conclusion: this array is palindrome");
         }
         else
-            System.out.println("this array is not a palindrome");
+            System.out.println("Conclusion: this array is not a palindrome");
     }
-    static int checkpalindrome(int[] arr, int start, int end) {
-        while (start <= end/2) {
+    static int checkPalindrome(int[] arr, int start, int end) {
             if (arr.length == 0 || arr.length == 1) {
                 return 1;
             }
-            else if (arr[start] == arr[end]) {
-                return checkpalindrome(arr, start + 1, end - 1);
+            if (arr[start] == arr[end] && start<=end) {
+                System.out.println("match with start = " + start + " and end = " + end);
+                return checkPalindrome(arr, start + 1, end - 1);
+            }
+            else if (arr[start] == arr[end] && start>end) {
+                return 1;
+            }
+            else if (arr[start] != arr[end] && start<=end) {
+                System.out.println("values with start = " + start + " and end = " + end + " do not match");
+                return 0;
             }
             else {
                 return 0;
             }
-        }
-        return 2;
     }
 }
